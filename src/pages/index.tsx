370ae1a9d2c8eac3cb44ec00666a7309dac828f5
lopes-joe <slopes.joe@gmail.com>
2021-10-19 10:16:38 -0300

    Criado Header da aplicação

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,17 @@
-import home from '../styles/home.module.scss'
+
+import Head from 'next/head'
+
 export default function Home(){
   return (
-    <blockquote id={home.quote}>
-      Confie em Jeová de todo o seu coração e não se estribe em sua própria compreensão. Nota-o em todos os seus caminhos, e Ele endireitará as suas veredas.
-      <br/>
-      <cite>Rei Salomão</cite>
-    </blockquote>
+    <>
+      <Head>
+        <title>Prov 3:4,5</title>
+      </Head>
+      <blockquote>
+        Confie em Jeová de todo o seu coração e não se estribe em sua própria compreensão. Nota-o em todos os seus caminhos, e Ele endireitará as suas veredas.
+        <br/>
+        <cite>— Rei Salomão</cite>
+      </blockquote>
+    </>
   )
 }
