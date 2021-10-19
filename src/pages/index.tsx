9a435fefb82ccb56425647f09542dfdc3cf95663
lopes-joe <slopes.joe@gmail.com>
2021-10-19 08:16:08 -0300

    Scoped CSS, SASS, arquivo _document

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,9 @@
-
+import home from '../styles/home.module.scss'
 export default function Home(){
   return (
-    <blockquote>
+    <blockquote id={home.quote}>
       Confie em Jeová de todo o seu coração e não se estribe em sua própria compreensão. Nota-o em todos os seus caminhos, e Ele endireitará as suas veredas.
+      <br/>
       <cite>Rei Salomão</cite>
     </blockquote>
   )
